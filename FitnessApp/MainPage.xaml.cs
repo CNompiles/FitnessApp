7cdf2c76a9501e46de55a142b7de6bb9ab1521c1
IliasSari <sarigilias@gmail.com>
2026-03-30 12:07:28 +0300

    Made some changes on Mainpage

--- FitnessApp/MainPage.xaml.cs
+++ FitnessApp/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace FitnessApp;
+﻿using FitnessApp.Models;
+using System.Net.Http.Json;
+
+namespace FitnessApp;
 
 public partial class MainPage : ContentPage
 {
@@ -9,16 +12,41 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnSearchClicked(object sender, EventArgs e)
-{
-    var foodName = FoodEntry.Text;
-    if (string.IsNullOrWhiteSpace(foodName))
+    public class FoodItem
     {
-        ResultLabel.Text = "Παρακαλώ γράψτε κάτι!";
-        return;
+        public string Name { get; set; } = string.Empty;
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fat { get; set; }
     }
+    private async void OnSearchClicked(object sender, EventArgs e)
+    {
+        var foodName = FoodEntry.Text;
+        if (string.IsNullOrWhiteSpace(foodName))
+        {
+            ResultLabel.Text = "Παρακαλώ γράψτε κάτι!";
+            return;
+        }
 
-    
-    ResultLabel.Text = $"Έφαγες {foodName}. Μπράβο!";
-}
+        ResultLabel.Text = "Αναζήτηση...";
+
+        try 
+        {
+            await Task.Delay(1000); 
+        
+            var mockResult = new FoodItem 
+            { 
+                Name = foodName, 
+                Calories = 150, 
+                Protein = 5 
+            };
+
+            ResultLabel.Text = $"{mockResult.Name}: {mockResult.Calories} θερμίδες και {mockResult.Protein}g πρωτεΐνης.";
+        }
+        catch (Exception ex)
+        {
+            ResultLabel.Text = "Σφάλμα κατά την αναζήτηση.";
+        }
+    }
 }
